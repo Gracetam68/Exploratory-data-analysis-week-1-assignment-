069a2be7f3f3b8dae90e78d8c953d4a1498df974
Grace <65880925+Gracetam68@users.noreply.github.com>
2020-08-07 13:43:33 -0400

    Update plot 2.R

--- plot 2.R
+++ plot 2.R
@@ -1,28 +1,31 @@
-## read table
-file <- read.table("household_power_consumption.txt", 
-                   header=TRUE, sep=";", na.strings = "?")
+## read the file 
+file2 <- read.table("household_power_consumption.txt", 
+                    header=TRUE, sep=";", na.strings = "?")
+##check file
+names(file2)
 
-## subset data from the dates 2007-02-01 and 2007-02-02
-sub <- subset(file, Date >=as.Date( "2007-02-01") &
-                       Date <= as.Date("2007-02-02"))
+## convert the Date and Time variables to Date/Time classes 
+file2$Date <- as.Date(file2$Date,"%d/%m/%Y")
 
-## convert the Date and Time variables to Date/Time classes
-sub$Date <- as.Date(sub$Date,"%d/%m/%Y")
+## extract the specific time period from 2007-02-01 to 2007-02-02
+sub <- subset(file2,Date >= as.Date("2007-02-01") & 
+                      Date <= as.Date("2007-02-02"))
 
-## combine Date and Time column
+## combine the date and time 
 dateTime <- paste(sub$Date,sub$Time)
-dateTime <- as.POSIXct(datetime)
-file1 <- cbind(dateTime, sub)
+dateTime <- as.POSIXct(dateTime)
+comb <- cbind(sub,dateTime)
 
 ## check and set up the margin
 par("mar")
 par(mar=c(4,4,4,4))
 
-## create histogram
-with(file1,{plot(file1$Global_active_power~file1$dateTime, type="l",
-                  xlab = " ",
-                  ylab="Global Active Power (kilowatts)")})
+## create plot
+with(comb,{plot(comb$dateTime,comb$Global_active_power, type="l",
+                xlab = " ",
+                ylab="Global Active Power(kilowatts)")})
 
 ## save to the file 
 dev.copy(png,"plot2.png",width=480, height=480)
 dev.off()
+
